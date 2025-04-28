f6042f9c74b702f0daf6d5cd562a5b38ae46bcb8
crisquii <crisquintana2207@gmail.com>
2025-04-27 23:30:30 -0500

    Cambio_1

--- Arbol.py
+++ Arbol.py
@@ -4,23 +4,95 @@ class Nodo:
         self.siguiente = None
         self.ramas = Lista()
 
+    def agregarRama(self,dataX,data):
+        if self.data == data:
+            self.ramas.insertarLista(dataX)
+            return True
+        else:
+            control = self.ramas.cabeza
+            while control != None:
+                if control.agregarRama(dataX, data):
+                    return True
+                control = control.siguiente
+            return False
+        
+    def contarNodos(self, contador):
+        contador = contador + 1
+        control = self.ramas.cabeza
+        while control != None:
+                contador = control.contarNodos(contador)
+                control = control.siguiente
+        return contador
+     
+    def sacarOrden(self, contador):
+        if contador < self.ramas.contar():
+            contador = self.ramas.contar()
+        control = self.ramas.cabeza
+        while control != None:
+                contador = control.sacarOrden(contador)
+                control = control.siguiente
+        return contador 
+
+    def sacarAltura(self, contador):
+        contador = contador + 1
+        altura = contador
+        control = self.ramas.cabeza
+        while control != None:
+                if altura < control.sacarAltura(contador):
+                    altura =  control.sacarAltura(contador)
+                control = control.siguiente
+        return altura 
+
 class Lista:
     def __init__(self):
         self.cabeza = None
-        
-    def insertarDespuesX(self, dato1, dato2):
+
+    def insertarLista(self, datoX):
         if self.cabeza == None:
-            return False
+            self.cabeza = Nodo(datoX)
         else:
             control = self.cabeza
-            while(control != None):
-                insertarDespuesX
+            while control.siguiente != None:
+                control = control.siguiente
+            control.siguiente = Nodo(datoX)
+
+    def contar(self):
+        contador = 0
+        control = self.cabeza
+        while control != None:
+                contador = contador + 1
+                control = control.siguiente
+        return contador
                    
 class Arbol:
     def __init__(self, data):
-        self.arbol = Lista()
-        self.arbol.insertarInicio(data)
+        self.raiz = Nodo(data)
         
-    def agregarNodo(self, data1, data2):
-        nuevo_nodo = Nodo(dato1)
-        self.arbol.cabeza.ramas.insertarDespues(data1, data2) 
+    def agregarNodo(self, dataX, data):
+        if self.raiz.agregarRama(dataX,data):
+            print("El nodo se agrego exitosamente.")
+        else:
+            print("Nose encontro el nodo suministrado.")
+
+    def peso(self):
+        contador = 0
+        print(f"El peso del arbol es: {self.raiz.contarNodos(contador)}")
+
+    def orden(self):
+        contador = 0
+        print(f"El orden del arbol es de: {self.raiz.sacarOrden(contador)}")
+
+    def altura(self):
+        contador = 0
+        print(f"La altura del arbol es: {self.raiz.sacarAltura(contador)}")
+
+
+
+arbol_1 = Arbol("hijo")
+arbol_1.agregarNodo("papa", "hijo")
+arbol_1.agregarNodo("mama", "hijo")
+arbol_1.agregarNodo("madrastra", "hijo")
+arbol_1.agregarNodo("abuela", "mama")
+arbol_1.peso()
+arbol_1.orden()
+arbol_1.altura()
